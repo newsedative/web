75f9e21f7f1182eb5f4108f03258011d3a17bd08
Darya <81254493+newsedative@users.noreply.github.com>
2024-07-23 16:12:10 +0300

    Update settings.py

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -44,6 +44,11 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
